7b44d0d360c36813e71646a8c0b2223b87bdcede
mfernandez-sisdam <mfernandez@loyal-solutions.com>
2018-02-09 17:14:04 -0300

    3er commit

--- src/main/java/com/mgf/init/Init.java
+++ src/main/java/com/mgf/init/Init.java
@@ -3,5 +3,13 @@ package com.mgf.init;
 public class Init {
 
 	private String init;
+
+	public String getInit() {
+		return init;
+	}
+
+	public void setInit(String init) {
+		this.init = init;
+	}
 	
 }
